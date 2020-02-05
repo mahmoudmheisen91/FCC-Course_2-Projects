e1f5be9c85a8facc54b1beb72048518ab7b0c2f9
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-05 08:33:43 +0200

    add multiple solutions to project 3

--- Scripts/caesarCipher.js
+++ Scripts/caesarCipher.js
@@ -4,10 +4,10 @@ function rot13(str) { // LBH QVQ VG!
         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     ];
 
-    let newStr = str.split("").map((item, index) => {
+    let newStr = str.split("").map(item => {
         let i = letterArray.indexOf(item);
         if (i >= 0) {
-            if (i - 13 < 0) i = i + 13;
+            if (i < 13) i = i + 13;
             else i = i - 13;
         } else return item;
         let elem = letterArray[i];
@@ -19,3 +19,40 @@ function rot13(str) { // LBH QVQ VG!
 
 // Change the inputs below to test
 console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
+
+function rot13_2(str) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str
+        .split('')
+        .map(char => {
+            const pos = alphabet.indexOf(char);
+            return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
+        })
+        .join('');
+}
+
+function rot13_3(str) {
+    // LBH QVQ VG!
+    return str.replace(/[A-Z]/g, L =>
+        String.fromCharCode((L.charCodeAt(0) % 26) + 65)
+    );
+}
+
+// Solution with Regular expression and Array of ASCII character codes
+function rot13_4(str) {
+    var rotCharArray = [];
+    var regEx = /[A-Z]/;
+    str = str.split("");
+    for (var x in str) {
+        if (regEx.test(str[x])) {
+            // A more general approach
+            // possible because of modular arithmetic
+            // and cyclic nature of rot13 transform
+            rotCharArray.push(((str[x].charCodeAt() - 65 + 13) % 26) + 65);
+        } else {
+            rotCharArray.push(str[x].charCodeAt());
+        }
+    }
+    str = String.fromCharCode.apply(String, rotCharArray);
+    return str;
+}
